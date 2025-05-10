ca2e85e7fbc299d045f8fd512bd50d167d6649ad
lucifron28 <lucifron28@gmail.com>
2025-05-10 20:32:47 +0800

    added logout button and functionality

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../app.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final _supabase = Supabase.instance.client;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profile'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            tooltip: 'Logout',
+            onPressed: () async {
+              await _supabase.auth.signOut();
+
+              if (context.mounted) {
+                Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) => const MyApp()),
+                      (route) => false,
+                );
+              }
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: const Text('Profile Screen'),
+      body: const Center(
+        child: Text('Profile Screen'),
       ),
     );
   }
