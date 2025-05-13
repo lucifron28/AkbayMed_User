a6e551441e24fdff7ec8ed922b7dbec2e922d7d6
lucifron28 <lucifron28@gmail.com>
2025-05-13 11:48:10 +0800

    selected expiration date async func

--- lib/screens/donation_screen.dart
+++ lib/screens/donation_screen.dart
@@ -30,4 +30,23 @@ class _DonationScreenState extends State<DonationScreen> {
     _expirationDateController.dispose();
     super.dispose();
   }
+
+  Future<void> _selectExpirationDate() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedExpirationDate ??
+          DateTime.now().add(const Duration(days: 30)),
+      firstDate: DateTime.now(),
+      lastDate: DateTime.now().add(const Duration(days: 3650)),
+    );
+
+    if (picked != null && picked != _selectedExpirationDate) {
+      setState(() {
+        _selectedExpirationDate = picked;
+        _expirationDateController.text =
+        "${picked.day}/${picked.month}/${picked.year}";
+      });
+    }
+  }
+
 }
