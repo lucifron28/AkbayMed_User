e26e0f6a65db4427972f4b89f24f407ea178904a
lucifron28 <lucifron28@gmail.com>
2025-05-14 20:32:07 +0800

    changed the request screen base to stateful widget

--- lib/screens/request_screen.dart
+++ lib/screens/request_screen.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 
-class RequestScreen extends StatelessWidget {
+class RequestScreen extends StatefulWidget {
   const RequestScreen({super.key});
 
+  @override
+  State<RequestScreen> createState() => _RequestScreenState();
+}
+
+class _RequestScreenState extends State<RequestScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Request'),
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(1.0),
-          child: Container(
-            color: Colors.black54,
-            height: 1.0,
-          ),
-        ),
       ),
       body: Center(
         child: const Text('Request Screen'),
