5aafc0a00742bc1fbb076348a767577a19f62ceb
Ron <lucifron28@gmail.com>
2025-05-15 20:00:35 +0800

    supbase instance declared & controllers

--- lib/screens/request_screen.dart
+++ lib/screens/request_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:logger/logger.dart';
+import 'package:http/http.dart';
 
 class RequestScreen extends StatefulWidget {
   const RequestScreen({super.key});
@@ -8,6 +11,18 @@ class RequestScreen extends StatefulWidget {
 }
 
 class _RequestScreenState extends State<RequestScreen> {
+  final _supabase = Supabase.instance.client;
+  final _logger = Logger();
+
+  final _medicineNameController = TextEditingController();
+  final _quantityController = TextEditingController();
+
+  bool _isLoading = false;
+  bool _isSearchingFDA = false;
+  DateTime? _selectedExpirationDate;
+  String? _selectedMedicationId;
+  List<String> _fdaSuggestions = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
